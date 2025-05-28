f571ca3ed9a713129b839baa7cd6e4b513075eaf
Ken-Smit <builtwithhandz@gmail.com>
2025-05-28 18:54:12 -0500

    styling

--- src/app/(shop)/orders/_layout.tsx
+++ src/app/(shop)/orders/_layout.tsx
@@ -1,8 +1,14 @@
 import { Stack } from 'expo-router'; 
 
 export default function OrdersLayout() {
-    return
-    <Stack>
-        <Stack.Screen name='index' options={{ headerShown: false, }} />
-    </Stack>
+    return (
+        <Stack>
+            <Stack.Screen
+                name='index'
+                options={{
+                    headerShown: false,
+                }}
+            />
+        </Stack>
+    );
 }
